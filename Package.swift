685609629eaef0dc6e299261252dca6f8692c55b
Hironobu Kimura <kimura@emotionale.jp>
2020-01-28 10:34:58 +0900

    add os version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "EMTNeumorphicView",
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "EMTNeumorphicView", targets: ["EMTNeumorphicView"])
     ],
